2ad5da924b808f8a1de65f0148bdf60a6f73c7c6
Fan DANG <i@dang.fan>
2026-06-19 16:59:20 +0800

    Fix WebAuthn settings flows

--- lib/views/starter_screen.dart
+++ lib/views/starter_screen.dart
@@ -4,9 +4,7 @@ import 'package:canokey_console/helper/utils/ui_mixins.dart';
 import 'package:canokey_console/helper/widgets/spacing.dart';
 import 'package:canokey_console/views/layout/layout.dart';
 import 'package:flutter/material.dart';
-import 'package:platform_detector/platform_detector.dart';
 import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class StarterScreen extends StatefulWidget {
   const StarterScreen({super.key});
@@ -15,7 +13,8 @@ class StarterScreen extends StatefulWidget {
   State<StarterScreen> createState() => _StarterScreenState();
 }
 
-class _StarterScreenState extends State<StarterScreen> with SingleTickerProviderStateMixin, UIMixin {
+class _StarterScreenState extends State<StarterScreen>
+    with SingleTickerProviderStateMixin, UIMixin {
   late StarterController controller;
 
   @override
@@ -42,11 +41,13 @@ class _StarterScreenState extends State<StarterScreen> with SingleTickerProvider
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(S.of(context).homePress, style: TextStyle(fontSize: 18.0)),
+                        Text(S.of(context).homePress,
+                            style: TextStyle(fontSize: 18.0)),
                         SizedBox(width: 5.0),
                         Icon(Icons.menu, color: Colors.black, size: 22.0),
                         SizedBox(width: 5.0),
-                        Text(S.of(context).homeSelect, style: TextStyle(fontSize: 18.0)),
+                        Text(S.of(context).homeSelect,
+                            style: TextStyle(fontSize: 18.0)),
                       ],
                     )
                   else
@@ -54,21 +55,10 @@ class _StarterScreenState extends State<StarterScreen> with SingleTickerProvider
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(S.of(context).homeDirectlySelect, style: TextStyle(fontSize: 18.0)),
+                        Text(S.of(context).homeDirectlySelect,
+                            style: TextStyle(fontSize: 18.0)),
                       ],
                     ),
-                  if (isWeb()) ...[
-                    SizedBox(height: 20),
-                    TextButton(
-                      onPressed: () {
-                        launchUrl(Uri.parse('https://console-legacy.canokeys.org'));
-                      },
-                      child: Text(
-                        'Use Legacy Version',
-                        style: TextStyle(fontSize: 16.0, decoration: TextDecoration.underline),
-                      ),
-                    ),
-                  ],
                 ],
               );
             }));
